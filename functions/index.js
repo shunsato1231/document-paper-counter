f67408401961791e3f2d30930dd415f52bb14b1b
sato-shn <sato-shn@indetail.co.jp>
2019-07-04 17:31:18 +0900

    複数の通知があった時に時間差で通知するよう修正

--- functions/index.js
+++ functions/index.js
@@ -8,53 +8,58 @@ admin.initializeApp(functions.config().firebase)
 const database = admin.firestore()
 const messaging = admin.messaging()
 
-function getNotificationDocument () {
-  return new Promise((resolve, reject) => {
-    database.collection('notificationList').get()
-      .then((querySnapshot) => {
-          let today = querySnapshot.docs.filter(doc => {
-            return moment().isSame(doc.data().deadline, 'day')
-          }).map(doc => {
-            return {
-              notificationKey: doc.data().notificationKey,
-              title: doc.data().title,
-              id: doc.id
-            }
-          })
-
-          let tomorrow = querySnapshot.docs.filter(doc => {
-            return moment().add(1, 'days').isSame(doc.data().deadline, 'day')
-          }).map(doc => {
-            return {
-              notificationKey: doc.data().notificationKey,
-              title: doc.data().title,
-              id: doc.id
-            }
-          })
-
-          let over = querySnapshot.docs.filter(doc => {
-            return moment().subtract(1, 'days').isSameOrAfter(doc.data().deadline, 'day')
-          }).map(doc => {
-            return {
-              notificationKey: doc.data().notificationKey,
-              title: doc.data().title,
-              id: doc.id
-            }
-          })
-
-        return resolve({today: today, tomorrow: tomorrow, over: over})
-      }).catch(reject)
-  })
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function pushNotificationTodaysDeadlineDocument(documents) {
-  return new Promise((resolve, reject) => {
-    const url = "https://fcm.googleapis.com/fcm/send"
-    documents.forEach(document => {
-      let headers = {
-        'Authorization': 'key=' + functions.config().vue_app.server_key,
-      }
+async function getNotificationDocument () {
+  try {
+    const querySnapshot = await database.collection('notificationList').get()
 
+    let today = querySnapshot.docs.filter(doc => {
+      return moment().isSame(doc.data().deadline, 'day')
+    }).map(doc => {
+      return {
+        notificationKey: doc.data().notificationKey,
+        title: doc.data().title,
+        id: doc.id
+      }
+    })
+
+    let tomorrow = querySnapshot.docs.filter(doc => {
+      return moment().add(1, 'days').isSame(doc.data().deadline, 'day')
+    }).map(doc => {
+      return {
+        notificationKey: doc.data().notificationKey,
+        title: doc.data().title,
+        id: doc.id
+      }
+    })
+
+    let over = querySnapshot.docs.filter(doc => {
+      return moment().subtract(1, 'days').isSameOrAfter(doc.data().deadline, 'day')
+    }).map(doc => {
+      return {
+        notificationKey: doc.data().notificationKey,
+        title: doc.data().title,
+        id: doc.id
+      }
+    })
+
+    return {today: today, tomorrow: tomorrow, over: over}
+  } catch (error) {
+    throw error
+  }
+}
+
+async function pushNotificationTodaysDeadlineDocument(documents) {
+  const url = "https://fcm.googleapis.com/fcm/send"
+  const headers = {
+    'Authorization': 'key=' + functions.config().vue_app.server_key,
+  }
+
+  try{
+    for(let document of documents) {
       let data = {
         "notification": {
           "title": "「" + document.title + "」は今日締め切りです",
@@ -64,21 +69,25 @@ function pushNotificationTodaysDeadlineDocument(documents) {
       }
 
       axios.post(url, data, {headers: headers, useCredentails: true})
-        .catch(reject)
-    })
-    resolve({'pushed today deadline document' : documents})
-  })
+
+      await sleep(2000)
+    }
+
+    return documents
+  } catch (error) {
+    throw error.response.status
+  }
+
 }
 
-function pushNotificationTomorrowDeadlineDocument(documents) {
-  return new Promise((resolve, reject) => {
-    let response = []
-    const url = "https://fcm.googleapis.com/fcm/send"
-    documents.forEach(document => {
-      let headers = {
-        'Authorization': 'key=' + functions.config().vue_app.server_key,
-      }
+async function pushNotificationTomorrowDeadlineDocument(documents) {
+  const url = "https://fcm.googleapis.com/fcm/send"
+  const headers = {
+    'Authorization': 'key=' + functions.config().vue_app.server_key,
+  }
 
+  try{
+    for(let document of documents) {
       let data = {
         "notification": {
           "title": "「" + document.title + "」は明日締め切りです",
@@ -88,32 +97,38 @@ function pushNotificationTomorrowDeadlineDocument(documents) {
       }
 
       axios.post(url, data, {headers: headers, useCredentails: true})
-        .catch(reject)
-    })
-    resolve({'pushed tommorow deadline document' : documents})
-  })
+
+      await sleep(2000)
+    }
+
+    return documents
+  } catch (error) {
+    throw error.response.status
+  }
 }
 
 function deleteOverDeadlineDocument(documents) {
-  return new Promise((resolve, reject) => {
+  try {
     documents.forEach(document => {
       database.collection('notificationList').doc(document.id).delete()
-        .catch(reject)
     })
-    resolve({'deleted document': documents})
-  })
+    return documents
+  } catch (error) {
+    throw error
+  }
 }
 
 
 //通知処理
-exports.pushNotification = functions.https.onRequest((request, response) => {
-  getNotificationDocument().then(res => {
-    return Promise.all([pushNotificationTodaysDeadlineDocument(res.today),
-                        pushNotificationTomorrowDeadlineDocument(res.tomorrow),
-                        deleteOverDeadlineDocument(res.over)])
-  }).then(res => {
-    return response.send(res)
-  }).catch(err => {
-    response.send(err)
-  })
+exports.pushNotification = functions.https.onRequest(async (request, response) => {
+  try{
+    const res = await getNotificationDocument()
+    const today = await pushNotificationTodaysDeadlineDocument(res.today)
+    const tomorrow = await pushNotificationTomorrowDeadlineDocument(res.tomorrow)
+    const deleteDocument = await deleteOverDeadlineDocument(res.over)
+
+    response.send({today: today, tomorrow: tomorrow, deleteDocument: deleteDocument})
+  } catch (error) {
+    response.status(500).send(error)
+  }
 })
